d58db7be6f5b1ba822bb66080794db76f43935f3
Adalid Duran Calderon <adalid.duran@outlook.com>
2026-01-16 11:06:12 -0600

    SMS-3 Database initial configuration

---
+++ StoreManagementService/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StoreManagementService.Models
+{
+    public partial class Item
+    {
+        public Item()
+        {
+            ItemsClientsRelationships = new HashSet<ItemsClientsRelationship>();
+            ItemsStoresRelationships = new HashSet<ItemsStoresRelationship>();
+        }
+
+        public Guid ItemId { get; set; }
+        public string ItemDescription { get; set; }
+        public decimal ItemPrice { get; set; }
+        public byte[] ItemImg { get; set; }
+        public int? ItemStock { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public virtual ICollection<ItemsClientsRelationship> ItemsClientsRelationships { get; set; }
+        public virtual ICollection<ItemsStoresRelationship> ItemsStoresRelationships { get; set; }
+    }
+}
---
+++ StoreManagementService/Models/OperationLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StoreManagementService.Models
+{
+    public partial class OperationLog
+    {
+        public int OperationId { get; set; }
+        public Guid? SessionId { get; set; }
+        public DateTime? OperationDate { get; set; }
+        public string Request { get; set; }
+        public string Response { get; set; }
+
+        public virtual SessionLog Session { get; set; }
+    }
+}
