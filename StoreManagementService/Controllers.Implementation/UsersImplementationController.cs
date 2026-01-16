b45844b4255aed4c3a66e8762971433d46b6e657
Adalid Duran Calderon <adalid.duran@outlook.com>
2026-01-16 13:58:57 -0600

    SMS-8 add update user method

--- StoreManagementService/Controllers.Implementation/UsersImplementationController.cs
+++ StoreManagementService/Controllers.Implementation/UsersImplementationController.cs
@@ -113,9 +113,30 @@ namespace StoreManagementService.Controllers.Implementation
 
         [HttpPut]
         [Route("~/{version::apiVersion}/Users/UpdateUser")]
-        public override Task<IActionResult> UpdateUser([FromRoute, RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$"), Required] string version, [Required] Guid userId, [Required] Guid sessionId, [Required] string currentPassword, string newPassword = null, string userName = null)
+        public async override Task<IActionResult> UpdateUser([FromRoute, RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$"), Required] string version, [Required] Guid userId, [Required] Guid sessionId, [Required] string currentPassword, string newPassword = null, string userName = null)
         {
-            return Task.FromResult<IActionResult>(Ok("AddUser"));
+            // Log the request
+            Dictionary<string, object> request = new Dictionary<string, object> { { "UpdateUserRequest", new object[] { "version: " + version, "userId: " + userId, "sessionId: " + sessionId, "newPassword: " + !String.IsNullOrEmpty(newPassword), "newUserName: " + !String.IsNullOrEmpty(userName) } } };
+            try
+            {
+                // Call the implementation
+                var result = await _userFunctionality.UpdateUser(userId, sessionId, currentPassword, newPassword, userName);
+                // Log the response
+                Dictionary<string, object> response = new Dictionary<string, object> { { "UpdateUserResponse", result } };
+                // Log the operation
+                await _serviceBaseFunctionality.LogOperation(request, response, sessionId);
+                // return the result
+                return Ok(new CustomResponse(statusCode: StatusCodes.Status200OK, message: result.Message, userId: result.UserId, sessionId: sessionId));
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Dictionary<string, object> response = new Dictionary<string, object> { { "ErrorUpdateUserResponse", ex } };
+                await _serviceBaseFunctionality.LogOperation(request, response, sessionId);
+                // if the exception is an OperationException, return a bad request with the error details
+                OperationException excep = ((OperationException)ex);
+                return this.BadRequest(new { StatusCode = StatusCodes.Status400BadRequest, code = excep.ErrorCode, message = excep.Message, details = excep.Details });
+            }
         }
     }
 }
